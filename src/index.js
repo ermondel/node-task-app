6f253b3f142aab3706a2b638534af8c2f410ba5d
ermondel <ermondel@gmail.com>
2019-05-18 14:35:10 +0300

    securely storing passwords: part 1

--- src/index.js
+++ src/index.js
@@ -11,3 +11,19 @@ app.use(userRouter);
 app.use(taskRouter);
 
 app.listen(PORT, () => { console.log('Server is up on port: ' + PORT); });
+
+//
+const bcrypt = require('bcryptjs');
+
+const myFunction = async () => {
+    const password = 'Red12345!';
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+    console.log(password);
+    console.log(hashedPassword);
+
+    const isMatch = await bcrypt.compare(password, hashedPassword);
+    console.log(isMatch);
+}
+
+myFunction();
