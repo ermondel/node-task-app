f6f67fae6970f4ee76c8a0f3d5e6eb8aa6e3c5de
ermondel <ermondel@gmail.com>
2019-05-20 19:49:16 +0300

    logging in users

--- src/index.js
+++ src/index.js
@@ -13,17 +13,17 @@ app.use(taskRouter);
 app.listen(PORT, () => { console.log('Server is up on port: ' + PORT); });
 
 //
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
 
-const myFunction = async () => {
-    const password = 'Red12345!';
-    const hashedPassword = await bcrypt.hash(password, 8);
+// const myFunction = async () => {
+//     const password = 'Red12345!';
+//     const hashedPassword = await bcrypt.hash(password, 8);
 
-    console.log(password);
-    console.log(hashedPassword);
+//     console.log(password);
+//     console.log(hashedPassword);
 
-    const isMatch = await bcrypt.compare(password, hashedPassword);
-    console.log(isMatch);
-}
+//     const isMatch = await bcrypt.compare(password, hashedPassword);
+//     console.log(isMatch);
+// }
 
-myFunction();
+// myFunction();
