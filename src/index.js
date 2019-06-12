958a1e39996a7fb01e69aa774f3bf5aff5d6aa46
ermondel <ermondel@gmail.com>
2019-06-12 09:36:08 +0300

    validating file uploads

--- src/index.js
+++ src/index.js
@@ -6,13 +6,23 @@ const taskRouter = require('./routers/task');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// const multer = require('multer');
-// const upload = multer({
-//     dest: 'images'
-// });
-// app.post('/upload', upload.single('upload'), (req, res) => {
-//     res.send();
-// });
+const multer = require('multer');
+const upload = multer({
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, callback) {
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return callback(new Error('Please upload a Word document'));
+        }
+        callback(undefined, true);
+    }
+});
+
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send();
+});
 
 app.use(express.json());
 app.use(userRouter);
