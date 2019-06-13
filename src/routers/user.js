aee084b8747367d9bf095271426114b124251fbc
ermondel <ermondel@gmail.com>
2019-06-13 21:04:25 +0300

    adding images to user profile

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,7 +4,6 @@ const User = require('../models/user');
 const auth = require('../middleware/auth');
 const multer = require('multer');
 const upload = multer({
-    dest: 'avatars',
     limits: {
         fileSize: 1000000
     },
@@ -95,10 +94,22 @@ router.delete('/users/me', auth, async (req, res) => {
     }
 });
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
     res.send();
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message });
+});
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    try {
+        req.user.avatar = undefined;
+        await req.user.save();
+        res.send();
+    } catch (e) {
+        res.send(500).send();
+    }
 });
 
 module.exports = router;
