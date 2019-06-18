a873bc08fe534142d6b9df0b6d7bc3c527057430
ermondel <ermondel@gmail.com>
2019-06-18 18:04:53 +0300

    sending welcome and cancelation emails

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,7 +3,8 @@ const router  = new express.Router();
 const User    = require('../models/user');
 const auth    = require('../middleware/auth');
 const multer  = require('multer');
-const sharp   = require('sharp'); 
+const sharp   = require('sharp');
+const { sendWelcomeEmail, sendCancelationEmail } = require('../emails/account');
 
 
 const upload = multer({
@@ -24,7 +25,11 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save();
+
+        sendWelcomeEmail(user.email, user.name);
+
         const token = await user.generateAuthToken();
+        
         res.status(201).send({ user, token });
     } catch (e) {
         res.status(400).send(e);
@@ -92,6 +97,9 @@ router.patch('/users/me', auth, async (req, res) => {
 router.delete('/users/me', auth, async (req, res) => {
     try {
         await req.user.remove();
+
+        sendCancelationEmail(req.user.email, req.user.name);
+
         res.send(req.user);
     } catch (e) {
         res.status(500).send();
